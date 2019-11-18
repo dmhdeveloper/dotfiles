94c7ea5cee4537fada3f154d1994a21cbb5bb50a
Devlin Hitchcock <devlin@entersekt.com>
2019-11-18 15:44:40 +0200

    Fix cpu and memory for mac

--- tmux/scripts/cpuandmem.sh
+++ tmux/scripts/cpuandmem.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-CPU=`eval $(awk '/^cpu /{print "previdle=" $5 "; prevtotal=" $2+$3+$4+$5 }' /proc/stat); sleep 0.4; eval $(awk '/^cpu /{print "idle=" $5 "; total=" $2+$3+$4+$5 }' /proc/stat); intervaltotal=$((total-${prevtotal:-0})); echo "$((100*( (intervaltotal) - ($idle-${previdle:-0}) ) / (intervaltotal) ))"`
-FREE_MEM=`free -m | awk 'NR==2' | awk '{print ((($2-$7) / $2)*100);}'`
+case `uname` in
+  Darwin)
+    CPU=`ps -A -o %cpu | awk '{ cpu += $1} END { printf cpu }'`
+    MEMORY_USED=`ps -A -o %mem | awk '{ mem += $1} END { printf mem }'`
+  ;;
+  Linux)
+    CPU=`eval $(awk '/^cpu /{print "previdle=" $5 "; prevtotal=" $2+$3+$4+$5 }' /proc/stat); sleep 0.4; eval $(awk '/^cpu /{print "idle=" $5 "; total=" $2+$3+$4+$5 }' /proc/stat); intervaltotal=$((total-${prevtotal:-0})); echo "$((100*( (intervaltotal) - ($idle-${previdle:-0}) ) / (intervaltotal) ))"`
+    MEMORY_USED=`free -m | awk 'NR==2' | awk '{print ((($2-$7) / $2)*100);}'`
+  ;;
+esac
 
-printf " ﬙ %.f%%   %.f%%" $CPU $FREE_MEM
+printf " ﬙ %.f%%   %.f%%" $CPU $MEMORY_USED
